cc5735ec462bd922f8694003227444b8f418a383
George Kando <gkando@gmail.com>
2019-05-08 15:39:25 -0500

    second commit

--- assignments/this.js
+++ assignments/this.js
@@ -21,19 +21,40 @@ const iSetIt = {
     greeting: 'Hello',
     foo: function(name, cb) {
       console.log(`${this.greeting} my name is ${name}`);
-      console.log("My scope is " + this.constructor.toString());
+      console.log(`My scope is: ${this.constructor.toString()}`);
       console.log(arguments.callee.name)
       iSetIt.tst(cb)
     },
-    tst: function() { console.log("My scope is: " + this.constructor.toString() + "\n I was called by: " + arguments.callee.caller.name) },
-    xyz: console.log("My scope is: " + this.constructor.name.toString())
-} 
-  iSetIt.foo('cat', "tst");
+    tst: function() { console.log(`My scope is: ${this.constructor.toString()} \n I was called by: ${arguments.callee.caller.name}`) },
+    xyz: console.log(`My scope is: ${this.constructor.name.toString()}`)
+}
+  iSetIt.foo("cat", "tst");
 
 // Principle 3
 
 // code example for New Binding
+function Foo() {        
+    this.name = 'Mike';
+    this.say = function () {
+    return "I am " + this.name; 
+   };
+}
+
+var name = 'Frank'; //WE DON'T CARE ABOUT YOU FRANK
+console.log('You think the name is going to be ' + name); 
+var result = new Foo();
+console.log('But its actually ' + result.name); //Mike wins the day
 
 // Principle 4
 
 // code example for Explicit Binding
+function greet() {
+	console.log(this.name);
+}
+
+var person = {
+	name:'Alex'
+};
+
+var greetPerson = greet.bind(person); 
+greetPerson();
